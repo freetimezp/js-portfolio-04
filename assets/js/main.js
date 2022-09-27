12dacb6579ba0b02cc0b265b19a8b98a51f65a39
freetimezp <kraftukrnet@ukr.net>
2022-09-27 21:24:07 +0300

    15. js-portfolio-04. Scroll style, show, hide scroll btn.

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,12 +98,35 @@ const sendEmail = (e) => {
 
 contactForm.addEventListener('submit', sendEmail);
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link');
+        }else{
+            sectionClass.classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUP = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUP.classList.add('show-scroll') : scrollUP.classList.remove('show-scroll');
+}
 
+window.addEventListener('scroll', scrollUp);
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
